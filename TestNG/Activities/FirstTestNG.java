d2790e226bc2ec6207f5d968c0626179007d8a57
yamini3106 <yamini.sailaja.kalneedi@ibm.com>
2026-02-02 13:17:31 +0530

    Add files via upload

---
+++ TestNG/Activities/FirstTestNG.java
@@ -0,0 +1,23 @@
+package example;
+
+import org.testng.annotations.Test;
+
+public class FirstTestNG {
+
+	@Test
+	public void One() {
+		System.out.println("this is one");
+	}
+	
+	@Test
+	public void Two() {
+		System.out.println("this is two");
+	}
+	
+	@Test
+	public void Three() {
+		System.out.println("this is three");
+	}
+	
+	
+}
